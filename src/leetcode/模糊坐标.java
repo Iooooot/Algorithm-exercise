507702582687a7f651ba32aaa6259d9ba9795e4c
wht <1369281736@qq.com>
2022-12-05 10:21:57 +0800

    feat: 完成leetcode：模糊坐标

---
+++ src/leetcode/模糊坐标.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+import java.util.Scanner;
+
+/**
+ * @author wht
+ * @date 2022/11/7 9:12
+ */
+public class 模糊坐标 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        str = str.substring(1,str.length()-1);
+        System.out.println(str);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < str.length(); i++) {
+
+        }
+    }
+}
